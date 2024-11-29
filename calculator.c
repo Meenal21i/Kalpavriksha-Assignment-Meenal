892552f5e17812351405c3bfc8cd62d137b44109
Meenal_Sharma <meenalsharma2003@gmail.com>
2024-11-29 09:33:15 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -5,114 +5,71 @@
 
 #define MAX 100
 
-int precedence(char op)
-{
-    if (op == '+' || op == '-')
-        return 1;   
-    if (op == '*' || op == '/')
-        return 2;
-    return 0;
+int applyOperation(int a, int b, char op) {
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': 
+            if (b == 0) { 
+                printf("Error: Division by zero.\n"); 
+                exit(1); 
+            }
+            return a / b;
+        default: return 0;
+    }
 }
 
-int applyOperation(int a, int b, char op)
-{
-    if (op == '+')
-        return a + b;
-    if (op == '-')
-        return a - b;
-    if (op == '*')
-        return a * b;
-    if (op == '/')
-    {
-        if (b == 0)
-        {
-            printf("Error: Division by zero.\n");
+void solve(char str[], int n) {
+    int stack[MAX], top = -1, num = 0;
+    char op = '+';
+
+    for (int i = 0; i < n; i++) {
+        if (isspace(str[i])) continue; 
+
+        if (isdigit(str[i])) {
+            num = num * 10 + (str[i] - '0');
+        } 
+
+        if (str[i] == '-' && (i == 0 || strchr("+-*/", str[i - 1]))) {
+            op = '-'; 
+            continue; 
+        }
+        
+        if (!isdigit(str[i]) && !isspace(str[i]) && !strchr("+-*/", str[i])) {
+            printf("Invalid character Error\n", str[i]);
             exit(1);
         }
-        return a / b;
+
+        
+        if (!isdigit(str[i]) || i == n - 1) {
+            if (op == '+') stack[++top] = num;
+            else if (op == '-') stack[++top] = -num;
+            else if (op == '*') stack[top] *= num;
+            else if (op == '/') {
+                if (num == 0) {
+                    printf("Error: Division by zero.\n");
+                    exit(1);
+                }
+                stack[top] /= num;
+            }
+            op = str[i];
+            num = 0;
+        }
     }
-    return 0;
+
+    int result = 0;
+    for (int i = 0; i <= top; i++) result += stack[i];
+    printf("Result: %d\n", result);
 }
 
-void solve(char str[], int n)
-{
-    int values[MAX], valueTop = -1; // Stack for numbers
-    char ops[MAX], opsTop = -1;     // Stack for operators
-
-    for (int i = 0; i < n; i++)
-    {
-        if (isspace(str[i]))
-            continue; // Ignore whitespace
-
-        if (isdigit(str[i]))
-        {
-            int val = 0;
-
-            // Read multi-digit numbers
-            while (i < n && isdigit(str[i]))
-            {
-                val = val * 10 + (str[i] - '0');
-                i++;
-            }
-            i--;
-            values[++valueTop] = val; // Push the number to the stack
-        }
-        else if (str[i] == '(')
-        {
-            ops[++opsTop] = str[i]; // Push '(' to the operators stack
-        }
-        else if (str[i] == ')')
-        {
-            while (opsTop >= 0 && ops[opsTop] != '(')
-            {
-                int b = values[valueTop--];
-                int a = values[valueTop--];
-                char op = ops[opsTop--];
-                values[++valueTop] = applyOperation(a, b, op);
-            }
-            if (opsTop >= 0 && ops[opsTop] == '(')
-                opsTop--; // Pop '('
-        }
-        else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
-        {
-            while (opsTop >= 0 && precedence(ops[opsTop]) >= precedence(str[i]))
-            {
-                int b = values[valueTop--];
-                int a = values[valueTop--];
-                char op = ops[opsTop--];
-                values[++valueTop] = applyOperation(a, b, op);
-            }
-            ops[++opsTop] = str[i]; // Push current operator
-        }
-        else
-        {
-            printf("Error: Invalid expression.\n");
-            exit(1);
-        }
-    }
-
-    // Apply remaining operations
-    while (opsTop >= 0)
-    {
-        int b = values[valueTop--];
-        int a = values[valueTop--];
-        char op = ops[opsTop--];
-        values[++valueTop] = applyOperation(a, b, op);
-    }
-
-    printf("Result: %d\n", values[valueTop]);
-}
-
-// Driver function
-int main()
-{
+int main() {
     char expression[MAX];
     printf("Enter a mathematical expression: ");
     fgets(expression, MAX, stdin);
 
     int length = strlen(expression);
-    if (expression[length - 1] == '\n')
-        expression[length - 1] = '\0'; // Remove newline
+    if (expression[length - 1] == '\n') expression[length - 1] = '\0';
 
     solve(expression, strlen(expression));
     return 0;
