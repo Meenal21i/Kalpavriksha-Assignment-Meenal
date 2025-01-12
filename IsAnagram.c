4929177899b8cc59981874cdf5569fdb3ffe14e7
Meenal_Sharma <meenalsharma2003@gmail.com>
2025-01-12 14:08:12 +0530

    Update IsAnagram.c
    Resolved naming conventions.
    Dynamic memory location for hash array.
    Removed unnecessary files.

--- IsAnagram.c
+++ IsAnagram.c
@@ -1,37 +1,44 @@
-#include <stdio.h>
-#include <stdbool.h>
-bool isAnagram(char s1[], char s2[])
-{
-    int hash1[26] = {0};
-    int hash2[26] = {0};
+/*
+ Problem Statement: 
+Write a program to check if two strings are anagrams of each other. Two strings are anagrams 
+if they contain the same characters in the same frequency. 
+Sample Input and Output: 
+1. Input: "listen", "silent" 
+Output: true 
+2. Input: "triangle", "integral" 
+Output: true 
+3. Input: "apple", "pale" 
+Output: false
+*/
 
-    for (int i = 0; s1[i] != '\0'; i++)
-    {
-        hash1[s1[i] - 'a']++;
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+bool isAnagram(char *str1, char *str2) {
+    int *hash = (int *)calloc(26, sizeof(int));
+    for(int index=0; str1[index] != '\0'; index++) {
+        hash[str1[index] - 'a']++;
+        hash[str2[index] - 'a']--;
     }
-    for (int i = 0; s2[i] != '\0'; i++)
-    {
-        hash2[s2[i] - 'a']++;
-    }
-    for (int i = 0; i < 26; i++)
-    {
-        if (hash1[i] != hash2[i])
-            return false;
+    for(int index=0; index<26; index++) {
+        if(hash[index] != 0 ) return false;
     }
     return true;
 }
 int main()
 {
-    char s1[10], s2[10];
-    scanf("%s", s1);
-    scanf("%s", s2);
-    if (isAnagram(s1, s2))
-    {
-        printf("True");
+    char *string1 = (char *)malloc(100 * sizeof(char));
+    scanf("%[^\n]", string1);
+    getchar();
+    char *string2 = (char *)malloc(100 * sizeof(char));
+    scanf("%[^\n]", string2);
+    
+    if(isAnagram(string1, string2)){
+        printf("true");
     }
-    else
-    {
-        printf("False");
+    else{
+        printf("false");
     }
+
     return 0;
 }
